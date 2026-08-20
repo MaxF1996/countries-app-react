6f22111990ee78087d06e32ceb6d43bc46679d1d
Max_F96 <maxeffect.apollo@gmail.com>
2026-08-21 00:39:29 +0300

    return from CountryPage by header button  fixed + mobile adapt for input

--- src/components/BackButton/BackButton.jsx
+++ src/components/BackButton/BackButton.jsx
@@ -1,6 +1,6 @@
-import classes from "./BackButton.module.css";
-import { Link } from "react-router-dom";
-import { FaArrowLeft } from "react-icons/fa6";
+import classes from './BackButton.module.css';
+import { Link } from 'react-router-dom';
+import { FaArrowLeft } from 'react-icons/fa6';
 
 export default function BackButton() {
   return (
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
-import classes from "./Header.module.css";
-import ThemeSwitcher from "../ThemeSwitcher/ThemeSwitcher";
-import { Link } from "react-router-dom";
+import classes from './Header.module.css';
+import ThemeSwitcher from '../ThemeSwitcher/ThemeSwitcher';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <header className={classes.Header}>
       <div className={classes.Header__Container}>
-        <Link to="./" className={classes.Header__Logo}>
+        <Link to="/" className={classes.Header__Logo}>
           Where in the world ?
         </Link>
         <ThemeSwitcher />
